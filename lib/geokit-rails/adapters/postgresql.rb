4bf9397197647537951815c7e92f7af717af89aa
Pol Miro <polmiro@gmail.com>
2014-02-21 15:52:25 -0800

    Fix postgres ignoring models without location

--- lib/geokit-rails/adapters/postgresql.rb
+++ lib/geokit-rails/adapters/postgresql.rb
@@ -1,22 +1,34 @@
 module Geokit
   module Adapters
     class PostgreSQL < Abstract
-      
+
       def sphere_distance_sql(lat, lng, multiplier)
         %|
-          (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
-          COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
-          SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
+          CASE WHEN #{qualified_lat_column_name} IS NULL OR #{qualified_lng_column_name} IS NULL
+          THEN
+            NULL
+          ELSE
+            (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
+            COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
+            SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
+          END
          |
       end
-      
+
       def flat_distance_sql(origin, lat_degree_units, lng_degree_units)
         %|
-          SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
-          POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
+          CASE WHEN #{qualified_lat_column_name} IS NULL OR #{qualified_lng_column_name} IS NULL
+          THEN
+            NULL
+          ELSE
+            #{lat} IS NOT NULL AND
+            #{lng} IS NOT NULL AND
+            SQRT(POW(#{lat_degree_units}*(#{origin.lat}-#{qualified_lat_column_name}),2)+
+            POW(#{lng_degree_units}*(#{origin.lng}-#{qualified_lng_column_name}),2))
+          END
          |
       end
-      
+
     end
   end
 end
